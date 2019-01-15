c1ab68983e779a00def9a02d48f09657299d32ec
shalini.ganesh <shalini.ganesh@boeing.com>
2019-01-15 21:44:47 +0530

    added comments

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,3 +1,14 @@
+/*Create a Main class in package com.stackroute and two Spring Beans – Movie, and Actor in
+package com.stackroute.domain.
+Actor has two String properties, name and gender, and an age property of type int.
+An Actor can be initialized with the three properties via the corresponding setter methods. Use
+property based injection in the bean definition file (beans.xml)
+Movie “has a” Actor that can be initialized via the corresponding setter method. Use property
+based object injection in the bean definition file (beans.xml)
+The Main class looks up Movie bean via three ways to print out actor information:
+1. Using XmlBeanFactory
+2. Using Spring 3.2 BeanDefinitionRegistry and BeanDefinitionReader
+3. Using ApplicationContext*/
 package com.stackroute.domain;
 
 public class Movie {
@@ -9,6 +20,7 @@ public class Movie {
     Movie(Actor actor){
         this.actor=actor;
     }
+    /*setter and getter method*/
     public Actor getActor() {
         return actor;
     }
--- src/main/java/com/stackroute/main.java
+++ src/main/java/com/stackroute/main.java
@@ -14,19 +14,19 @@ import org.springframework.core.io.ClassPathResource;
 
 public class main {
     public static void main(String[] args) {
+        /*used XmlBeanFactory method*/
         BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
         Movie movie2=(Movie) beanFactory.getBean("movieXYZ");
         System.out.println(movie2.getActor());
-
+        /*used  BeanDefinitionRegistry and BeanDefinitionReader method*/
         BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
         BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
         beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
         Movie movie3=(Movie)((DefaultListableBeanFactory) beanDefinitionRegistry).getBean("movieXYZ");
         System.out.println(movie3.getActor());
-
+        /*used ApplicationContext method */
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
         Movie movie4=(Movie)context.getBean("movieXYZ");
         System.out.println(movie4.getActor());
-
     }
 }
